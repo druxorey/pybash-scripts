45bce563de650d591c4db0f81993f282b4718fd5
Druxorey <guillermogalavisg@gmail.com>
2024-02-21 19:38:44 -0400

    Modified the code by removing unnecessary repetition and replacing it with a '+='

--- Terminal Interface/Mastermind/8-ysy_test.py
+++ Terminal Interface/Mastermind/8-ysy_test.py
@@ -15,9 +15,9 @@ print("""
 answer1 = str(input("Responde la letra de la opción que creas correcta: "))
 answer1 = answer1.lower()
 if answer1 == "a" or answer1 == "c":
-    score = score + 0
-if answer1 == "b":
-    score = score + 10
+    score += 0
+elif answer1 == "b":
+    score += 10
 
 print("Tu puntaje actual es: ", score)
 
@@ -34,9 +34,9 @@ print("""
 answer2 = str(input("Responde la letra de la opción que creas correcta: "))
 answer2 = answer2.lower()
 if answer2 == "a" or answer2 == "b" or answer2 == "c" or answer2 == "d":
-    score = score + 5
-if answer2 == "e":
-    score = score + 10
+    score += 5
+elif answer2 == "e":
+    score += 10
 
 print("Tu puntaje actual es: ", score)
 
@@ -51,17 +51,17 @@ print("""
 answer3 = str(input("Responde la letra de la opción que creas correcta: "))
 answer3 = answer3.lower()
 if answer3 == "a":
-    score = score + 0
-if answer3 == "b":
-    score = score + 5
-if answer3 == "c":
-    score = score + 10
+    score += 0
+elif answer3 == "b":
+    score += 5
+elif answer3 == "c":
+    score += 10
 
 print(f"Muy bien {name} tu puntuación final es: {score}")
 
 if score == 30:
     print("ERES EL VERDADERO FAN DEL YSY A")
-if 5 < score < 30:
+elif 5 < score < 30:
     print("Está bien, te gusta YSY")
-if score <= 5:
+elif score <= 5:
     print("Fan por moda")
