1e6e214e861d0ed97e54eaa65f97e4aa052c4638
Druxorey <guillermogalavisg@gmail.com>
2024-02-22 21:46:21 -0400

    The location and structure of the directories has been modified

--- terminal_interface/Mastermind/7-zoo_discount.py
+++
@@ -1,9 +0,0 @@
-age = int(input("¿Cuál es tu edad?: "))
-carnet = str(input("¿Qué tipo de carnet tienes? (E, P, F, N): "))
-
-carnet = carnet.lower()
-
-if (25 <= age <= 35 and carnet == "e") or (age <= 10) or (age >= 65 and carnet == "p") or (carnet == "f"):
-    print("sexo")
-else:
-    print("nosexo")
