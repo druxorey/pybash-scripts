ab96812896790fb86c7f6779e04ac52b853dd406
Druxorey <guillermogalavisg@gmail.com>
2024-02-26 17:51:35 -0400

    Improved the score data display and added the potions feature

--- python/terminal/games/maze-snake/maze-snake.py
+++ python/terminal/games/maze-snake/maze-snake.py
@@ -11,6 +11,7 @@ POS_Y = 1
 
 COLOR_CHARACTER = (Fore.YELLOW)
 COLOR_APPLE = (Fore.RED)
+COLOR_POTION = (Fore.GREEN)
 COLOR_WALL = (Fore.BLUE)
 COLOR_RESET = (Style.RESET_ALL)
 
@@ -30,14 +31,20 @@ character_position = [12,12]
 tail_length = 1
 tail = []
 
-# Initial objects values
-object_quantity = random.randint(4,8)
-object_main_list = []
-map_objects = object_main_list
+# Initial apples values
+apple_quantity = random.randint(4,8)
+apple_main_list = []
+map_apples = apple_main_list
+
+# Initial potions values
+potion_quantity = random.randint(1,3)
+potion_main_list = []
+map_potions = potion_main_list
 
 # Initial game values
 level = 0
 score = 0
+
 
 # Map
 obstacle_definition = """\
@@ -81,17 +88,29 @@ while True:
 
     os.system("clear")
 
-    # generate random objects in the map
-    if len(map_objects) <= 0:
-        object_quantity += 1
+    # generate random apples in the map
+    if len(map_apples) <= 0:
+        apple_quantity += 1
         level += 1
-        for _ in range(object_quantity):
+        for _ in range(apple_quantity):
             while True:
-                new_object_position = [random.randint(0,(MAP_WIDTH - 1 )),random.randint(0,(MAP_HEIGHT - 1 ))]
+                new_apple_position = [random.randint(0,(MAP_WIDTH - 1 )),random.randint(0,(MAP_HEIGHT - 1 ))]
                 # Check if the new position is a wall
-                if obstacle_definition[new_object_position[POS_Y]][new_object_position[POS_X]] != "#":
-                    object_main_list.append(new_object_position)
+                if obstacle_definition[new_apple_position[POS_Y]][new_apple_position[POS_X]] != "#":
+                    apple_main_list.append(new_apple_position)
                     break
+        # generate random potions in the map
+        if len(map_potions) <= 0:
+            potion_quantity += 1
+            for _ in range(potion_quantity):
+                while True:
+                    new_potion_position = [random.randint(0,(MAP_WIDTH - 1 )),random.randint(0,(MAP_HEIGHT - 1 ))]
+                    # Check if the new position is a wall
+                    if obstacle_definition[new_potion_position[POS_Y]][new_potion_position[POS_X]] != "#":
+                        potion_main_list.append(new_potion_position)
+                        break
+    
+
 
     print(f"{COLOR_RESET}███" + "███" * MAP_WIDTH  + "███")
     for coordinate_y in range(MAP_HEIGHT):
@@ -99,16 +118,23 @@ while True:
         for coordinate_x in range(MAP_WIDTH):
 
             char_to_draw = "   "
-            object_in_cell = None
+            apple_in_cell = None
+            potion_in_cell = None
 
             if obstacle_definition[coordinate_y][coordinate_x] == "#":
                 char_to_draw = f"{COLOR_WALL}███"
 
-            for map_object in map_objects:
+            for map_apple in map_apples:
 
-                if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
+                if map_apple[POS_X] == coordinate_x and map_apple[POS_Y] == coordinate_y:
                     char_to_draw = f"{COLOR_APPLE} 󰉛 "
-                    object_in_cell = map_object
+                    apple_in_cell = map_apple
+            
+            for map_potion in map_potions:
+
+                if map_potion[POS_X] == coordinate_x and map_potion[POS_Y] == coordinate_y:
+                    char_to_draw = f"{COLOR_POTION}  "
+                    potion_in_cell = map_potion
 
             for tail_piece in tail:
                 if tail_piece[POS_X] == coordinate_x and tail_piece[POS_Y] == coordinate_y:
@@ -117,26 +143,40 @@ while True:
             if character_position[POS_X] == coordinate_x and character_position[POS_Y] == coordinate_y:
                 char_to_draw = f"{COLOR_CHARACTER}  "
                 
-                if object_in_cell:
-                    map_objects.remove(object_in_cell)
+                if apple_in_cell:
+                    map_apples.remove(apple_in_cell)
                     tail_length += 1
                     score += 1
 
+                if potion_in_cell:
+                    map_potions.remove(potion_in_cell)
+                    tail_length -= 1
+                    score += 5
 
             print(f"{char_to_draw}", end="")
         
         print(f"{COLOR_RESET}███")
 
     print("███" + "███" * MAP_WIDTH  + "███")
+    print(f"\n")
 
     # User data
-    print(f"Nivel: {level}")
-    print(f"Puntaje: {score}")
-        
+    level_message = f"   Nivel: {str(level).zfill(2)}   "
+    score_message = f"Puntuación: {str(score).zfill(3)}"
+
+    data_length = int((len(level_message) + len(score_message)) / 4)
+    data_visual = ("   " * (int(MAP_WIDTH / 2) - int(data_length / 2))) 
+
+    print("███" + "███" * MAP_WIDTH  + "███")
+    print("███" + data_visual + "   " + score_message + "   " + data_visual + "███")
+    print("███" + data_visual + "   " + level_message + "   " + data_visual + "███")
+    print("███" + "███" * MAP_WIDTH  + "███")
+
     # Debug mode
     if debug_mode == True:
-        print("Ubicación de los objetos del mapa: ",map_objects)
-        print("Objetos del mapa: ",len(map_objects))
+        print("Ubicación de las manzanas del mapa: ",map_apples)
+        print("Ubicación de las pociones del mapa: ",map_potions)
+        print("Objetos del mapa: ",len(map_apples) + len(map_potions))
         print("Posición del personaje: ",character_position)
         print("Largo de la cola: ",tail_length)
 
@@ -182,7 +222,7 @@ while True:
     for tail_piece in tail[1:]:  
         if tail_piece[POS_X] == character_position[POS_X] and tail_piece[POS_Y] == character_position[POS_Y]:
             os.system("clear")
-            print("¡Has muerto por puta!")
+            print("¡Has perdido!")
             print(f"Has llegado al nivel: {level}")
             print(f"Tu puntaje fue de: {score}")
             exit()
