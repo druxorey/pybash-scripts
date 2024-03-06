cd69f37d9999b995068a1dd22e2541718d8ffe1c
Druxorey <guillermogalavisg@gmail.com>
2024-03-06 15:53:47 -0400

    Update Pokemon battle game codebase.
    - Updated Pokemon data files.
    - Adjusted type effectiveness calculations.
    - Improved the user interface for choosing a Pokemon.
    - Added a minimum level requirement for attacks.
    - Fixed a bug where enemy attacks were not always effective.
    - Improved the formatting and display of information during battles.

--- python/terminal/games/pykebattle/scripts/visuals.py
+++ python/terminal/games/pykebattle/scripts/visuals.py
@@ -38,16 +38,37 @@ def print_inside_box(string):
 
 
 def print_pokemon_information(player_pokemon, enemy_pokemon):
+    os.system("clear")
     print("╔" + ("═" * SCREEN_WIDTH) + "╗")
     print("║" + (colorize(player_pokemon, "G") + "  VS  " + colorize(enemy_pokemon, "R")).center(SCREEN_WIDTH + 18) + "║")
     print("╚" + ("═" * SCREEN_WIDTH) + "╝")
 
 
+def print_attacks(player_pokemon):
+    attacks_to_print = [attack for attack in player_pokemon["attacks"] if int(attack["min_level"]) <= int(player_pokemon["level"])]
+    print("╔" + ("═" * SCREEN_WIDTH) + "╗")
+    print("║" + ("MOVIMIENTOS").center(SCREEN_WIDTH) + "║")
+    print("║" + (" " * SCREEN_WIDTH) + "║")
+
+    chosen = None
+    while not chosen:
+        for index in range(len(attacks_to_print)):
+            print("║" + ("{:<2} - {} ".format(index, get_move_info(attacks_to_print[index]))).center(SCREEN_WIDTH) + "║")
+        try:
+            print("║" + (" " * SCREEN_WIDTH) + "║")
+            print("╚" + ("═" * SCREEN_WIDTH) + "╝")
+            return attacks_to_print[int(input("¿Cuál eliges?\n"))]
+        except (ValueError, IndexError):
+            # clear_and_header("", "", False)
+            print("¡¡¡Opcion invalida!!!")
+
+
 def get_pokemon_info(pokemon):
     return "{:<10} │ lvl {:<2} │ HP {}/{}".format(pokemon["name"], pokemon["level"], pokemon["current_health"], pokemon["base_health"])
 
+
 def get_move_info(attack):
-    return " {} || {} DP || Tipo {}".format(attack["name"], attack["damage"], attack["type"])
+    return " {:<15} || {:<3} DP || Tipo {:<8}".format(attack["name"], attack["damage"], attack["type"])
 
 
 def print_actions():
