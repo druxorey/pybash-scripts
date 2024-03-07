57f6e4e0118884f11c4aedf7c9638f310c9d4bc7
Druxorey <guillermogalavisg@gmail.com>
2024-03-06 20:28:00 -0400

    The attack function has been modified, eliminating code repetition and encompassing everything in a single function, making it easier to modify the information messages about the attacks carried out by the Pokémon as well as who is doing them.

--- python/terminal/games/pykebattle/scripts/attack.py
+++ python/terminal/games/pykebattle/scripts/attack.py
@@ -7,15 +7,14 @@ from data import multiplier_for_type
 from .visuals import colorize, print_pokemon_information, get_pokemon_info, print_attacks
 
 
-def player_attack(player_pokemon, enemy_pokemon):
-    print_pokemon_information(get_pokemon_info(player_pokemon), (get_pokemon_info(enemy_pokemon)))
-    multiplier = multiplier_for_type(player_pokemon, enemy_pokemon)
-    attack = print_attacks(player_pokemon)
-    print_pokemon_information(get_pokemon_info(player_pokemon), (get_pokemon_info(enemy_pokemon)))
-
-    print("{} ataca con: {}\n".format(colorize(player_pokemon["name"], "G"), attack["name"]))
-    time.sleep(2)
-
+def attack_information(attacking_pokemon, defending_pokemon, multiplier, attack, is_player):
+    if is_player:
+        attacking_pokemon_color = "G"
+        defending_pokemon_color = "R"
+    else:
+        attacking_pokemon_color = "R"
+        defending_pokemon_color = "G"
+    print("{} ataca con: {}\n".format(colorize(attacking_pokemon["name"], attacking_pokemon_color), attack["name"]))
     if multiplier > 1:
         print("El ataque fue super efectivo")
         time.sleep(1)
@@ -23,46 +22,33 @@ def player_attack(player_pokemon, enemy_pokemon):
         print("El ataque no ha sido muy efectivo")
         time.sleep(1)
 
-    damage = (int(attack["damage"]) + player_pokemon["attack"] - enemy_pokemon["defense"]) * multiplier
-    enemy_pokemon["current_health"] = int(enemy_pokemon["current_health"]) - damage
+    damage = (int(attack["damage"]) + attacking_pokemon["attack"] - defending_pokemon["defense"]) * multiplier
+    defending_pokemon["current_health"] = int(defending_pokemon["current_health"]) - damage
 
-    print("{} recibe {} puntos de daño (DP)".format(colorize(enemy_pokemon["name"], "R"), damage))
+    print("{} recibe {} puntos de daño (DP)".format(colorize(defending_pokemon["name"], defending_pokemon_color), damage))
     time.sleep(2)
 
-    if int(enemy_pokemon["current_health"]) <= 0:
-        enemy_pokemon["current_health"] = 0
-        print("{} está fuera de combate.".format(colorize(enemy_pokemon["name"], "R")))
+    if int(defending_pokemon["current_health"]) <= 0:
+        defending_pokemon["current_health"] = 0
+        print("{} está fuera de combate.".format(colorize(defending_pokemon["name"], defending_pokemon_color)))
 
+
+def player_attack(player_pokemon, enemy_pokemon):
+    
+    multiplier = multiplier_for_type(player_pokemon, enemy_pokemon)
+    attack = print_attacks(player_pokemon, enemy_pokemon)
+    
+    print_pokemon_information(get_pokemon_info(player_pokemon), (get_pokemon_info(enemy_pokemon)))
+    attack_information(player_pokemon, enemy_pokemon, multiplier, attack, is_player=True)
+    
 
 def enemy_attack(enemy_pokemon, player_pokemon):
     if int(enemy_pokemon["current_health"]) > 0:
 
-        print_pokemon_information(get_pokemon_info(player_pokemon), (get_pokemon_info(enemy_pokemon)))
         multiplier = multiplier_for_type(enemy_pokemon, player_pokemon)
-
         attacks = [attack for attack in enemy_pokemon["attacks"] if
                    int(attack["min_level"]) <= int(enemy_pokemon["level"])]
-
         attack = random.choice(attacks)
 
-        print("{} ataca con: {}\n".format(colorize(enemy_pokemon["name"], "R"), attack["name"]))
-        time.sleep(2)
-
-        if multiplier > 1:
-            print("El ataque fue super efectivo")
-            time.sleep(1)
-        elif multiplier < 1:
-            print("El ataque no ha sido muy efectivo")
-            time.sleep(1)
-
-        damage = (int(attack["damage"]) + enemy_pokemon["attack"] - player_pokemon["defense"]) * multiplier
-        player_pokemon["current_health"] = int(player_pokemon["current_health"]) - damage
-
-        print("{} recibe {} puntos de daño (DP)".format(colorize(player_pokemon["name"], "G"), damage))
-        time.sleep(2)
-
-        if int(player_pokemon["current_health"]) <= 0:
-            player_pokemon["current_health"] = 0
-            print("{} está fuera de combate.".format(colorize(player_pokemon["name"], "G")))
-
         print_pokemon_information(get_pokemon_info(player_pokemon), (get_pokemon_info(enemy_pokemon)))
+        attack_information(enemy_pokemon, player_pokemon, multiplier, attack, is_player=False)
