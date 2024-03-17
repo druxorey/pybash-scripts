c073f4ce44b4c854d2bec296510804536ca1bf4d
Druxorey <guillermogalavisg@gmail.com>
2024-03-17 01:09:28 -0400

    Added configuration file for easier handling of initial variables and game settings.

---
+++ python/terminal/games/maze-snake/config.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Style
+
+WALL = "███"
+SPACE = "   "
+
+COLOR_CHARACTER = (Fore.YELLOW)
+COLOR_APPLE = (Fore.RED)
+COLOR_POTION = (Fore.GREEN)
+COLOR_WALL = (Fore.BLUE)
+COLOR_RESET = (Style.RESET_ALL)
