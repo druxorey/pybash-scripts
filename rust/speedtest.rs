dd54b42827ecb9add3db462723a5a89752609798
Druxorey <guillermogalavisg@gmail.com>
2024-05-04 16:07:40 -0400

    New minor scripts has been created.

---
+++ rust/speedtest.rs
@@ -0,0 +1,16 @@
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+
+    let mut count = 0;
+    let number = 50_000_000;
+
+    for _i in 0..number {
+        count += 1;
+    }
+
+    let duration = start.elapsed();
+
+    println!("Counting to {} in Rust took {:?} seconds", number, duration.as_secs_f64());
+}
