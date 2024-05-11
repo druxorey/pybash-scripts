80e14922a183f8285c5d420e4019ee232afb43cb
Druxorey <guillermogalavisg@gmail.com>
2024-05-11 02:14:30 -0400

    The c++ speedtest has been created.

---
+++ c++/speedtest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int count=0, number=50000000;
+
+    for(int i=0; i<=number; i++){
+        count += 1;
+    }
+
+    cout << "done" << endl;
+
+    return 0;
+}
