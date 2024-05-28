70a912f7675dca661495ff7465a05d859baf93f2
Druxorey <guillermogalavisg@gmail.com>
2024-05-28 00:24:55 -0400

    feat(c++): existing second-practice scripts variants.

---
+++ c++/practices/second-practice/pirate-coordinates.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int firstNumber, secondNumber, thirdNumber;
+	int firstX, firstY, secondX, secondY, thirdX, thirdY;
+
+	cout << "Enter the first number: ";
+	cin >> firstNumber;
+	cout << "Enter the second number: ";
+	cin >> secondNumber;
+	cout << "Enter the third number: ";
+	cin >> thirdNumber;
+
+	firstX = firstNumber / 100;
+	firstY = firstNumber % 100;
+
+	secondX = secondNumber / 100;
+	secondY = secondNumber % 100;
+
+	thirdX = thirdNumber / 100;
+	thirdY = thirdNumber % 100;
+
+	int xFinal, yFinal;
+
+	xFinal = (firstX + secondX + thirdX) / 3;
+	yFinal = (firstY + secondY + thirdY) / 3;
+
+	cout << "The final coordinate is X:" << xFinal << " Y:" << yFinal << endl;
+
+	return 0;
+}
