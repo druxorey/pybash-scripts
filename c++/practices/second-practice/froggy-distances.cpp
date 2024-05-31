518d36a9b1511851775a261f8864971d0f4ce0fa
Druxorey <guillermogalavisg@gmail.com>
2024-05-30 23:54:19 -0400

    fix(c++): froggy-distance conditional fixed.

--- c++/practices/second-practice/froggy-distances.cpp
+++ c++/practices/second-practice/froggy-distances.cpp
@@ -3,26 +3,22 @@
 using namespace std;
 
 int main(){
-    int joseJumps, pedroJumps, joseTimes, totalDistance, totalPene;
-
-    cout << endl << "Jose and Pedro are two frogs. Jose can jump a number of centimeters and Pedro can jump a number of centimeters. Assuming that both frogs start jumping at the same point (and in the same direction), will the frogs coincide at the same point at some point before Jose makes a certain number of jumps?" << endl << endl;
+    int joseDistance, pedroDistance, joseJumps;
 
     cout << "Enter the distance Jose can jump: ";
-    cin >> joseJumps;
+    cin >> joseDistance;
 
     cout << "Enter the times Jose will jump: ";
-    cin >> joseTimes;
+    cin >> joseJumps;
 
     cout << "Enter the distance Pedro can jump: ";
-    cin >> pedroJumps;
+    cin >> pedroDistance;
     cout << endl;
 
-    totalDistance = pedroJumps * joseTimes;
-
-    if ((totalDistance % joseJumps) == 0){
-        cout << "The frogs will meet at the same point at some point before Jose finishes all his jumps." << endl;
+    if ( (joseDistance * pedroDistance) <= (joseDistance * joseJumps) ){
+        cout << "The frogs will meet at the same point before Jose finishes all his jumps." << endl;
     } else{
-        cout << "The frogs will not meet at the same point at some point before Jose finishes all his jumps." << endl;
+        cout << "The frogs will not meet at some point before Jose finishes all his jumps." << endl;
     }
 
     return 0;
